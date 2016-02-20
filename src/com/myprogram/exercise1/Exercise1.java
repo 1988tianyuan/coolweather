439a0735ba99c5d81c0a03a3472e639f8ad44e6c
Liugeng <467651794@qq.com>
2016-02-20 20:49:12 +0800

    First commit

---
+++ src/com/myprogram/exercise1/Exercise1.java
@@ -0,0 +1,17 @@
+package com.myprogram.exercise1;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+
+public class Exercise1 extends Activity{
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.exercise1); 
+
+	}
+
+	
+}
